69f2730169ec516e2448e15498c16ce375d58f4e
Matheus Gomes <matheusgomes062@gmail.com>
2022-02-24 13:49:50 -0300

    feat: profile white line

--- src/components/About.vue
+++ src/components/About.vue
@@ -42,30 +42,11 @@ section(class="card-wrap" aria-labelledby="about-title" role="region" ref="card"
           i-mdi-dev-to
         a(v-if="social.medium" aria-label="medium" :href="social.medium" class="link link--icon text-3xl  mr-2 p-0")
           i-mdi-medium
-
-    //- div(class="profile-container hexagon")
+    div(class="profile-background-line")
     div(class="profile")
 </template>
 
 <style scoped lang="scss">
 @import './styles/about.scss';
 
-.cool-gradient-title {
-background-image: linear-gradient(
-  90deg,
-  hsl(193deg 71% 56%) 0%,
-  hsl(197deg 100% 46%) 11%,
-  hsl(204deg 100% 48%) 22%,
-  hsl(211deg 100% 49%) 33%,
-  hsl(219deg 100% 47%) 44%,
-  hsl(249deg 75% 56%) 56%,
-  hsl(260deg 76% 55%) 67%,
-  hsl(269deg 77% 53%) 78%,
-  hsl(278deg 77% 50%) 89%,
-  hsl(286deg 100% 44%) 100%
-);
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-}
 </style>
