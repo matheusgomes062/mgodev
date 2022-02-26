de3a760476ec94184c71147398dceaacf35dd7e8
Matheus Gomes <matheusgomes062@gmail.com>
2022-02-26 05:32:21 -0300

    feat: skills that I know, that i want to know, and that I'm learning

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -9,16 +9,24 @@ section(class="section skills-section" id="skills" aria-labelledby="skills-title
   h2(class="section__title font-bold" id="skills-title") {{ t('intro.skills') }}
   div(class="skills-section__container")
     ul(class="max-w-1100px w-full mx-auto flex flex-wrap justify-center")
-      li(class="m-2" v-for="skill in skills")
-        div(class="btn btn--plain flex flex-row items-center rounded-md section__skill")
+      h3(class="w-full text-center mb-3") {{t('using_or_used_before')}}
+      li(v-for="skill in skills")
+        div(class="m-2 btn btn--plain flex flex-row items-center rounded-md section__skill" v-if="!skill.learning && skill.wantsToLearn === undefined")
+          p(class="mr-3") {{ skill.name }}
+          Icon(:icon="skill.icon" height="24")
+      h3(class="w-full text-center mt-10 mb-3") {{ t('learning_right_now') }}
+      li(v-for="skill in skills")
+        div(class="m-2 btn btn--plain flex flex-row items-center rounded-md section__skill" v-if="skill.learning")
+          p(class="mr-3") {{ skill.name }}
+          Icon(:icon="skill.icon" height="24")
+      h3(class="w-full text-center mt-10 mb-3") {{ t('wants_to_learn') }}
+      li(v-for="skill in skills")
+        div(class="m-2 btn btn--plain flex flex-row items-center rounded-md section__skill" v-if="skill.wantsToLearn")
           p(class="mr-3") {{ skill.name }}
           Icon(:icon="skill.icon" height="24")
 </template>
 
 <style scoped lang="scss">
-
-$hoverEasing: cubic-bezier(0.23, 1, 0.32, 1);
-$returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
 
 @keyframes spin {
   0% {
@@ -35,9 +43,6 @@ $returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
   inherits: false;
 }
 
-.skills-section {
-}
-
 .skills-section__container {
   background-color: var(--clr-bg-alt);
   border-radius: 1em;
@@ -46,18 +51,19 @@ $returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
 
   li {
     transform-style: preserve-3d;
+    background: none;
 
     &:hover:before {
       content: "";
       position: absolute;
       z-index: -1;
-      width: 104%;
-      height: 110%;
-      border-radius: 9px;
-      filter: blur(9px);
+      width: 95%;
+      height: 90%;
+      border-radius: 1em;
+      filter: blur(10px);
       background-image: linear-gradient( var(--rotate) , #5ddcff, #3c67e3 43%, #4e00c2);
-      top: -4%;
-      left: 0%;
+      top: 1%;
+      left: 3%;
       opacity: 0.5;
       animation: spin 2.5s linear infinite;
     }
@@ -66,5 +72,11 @@ $returnEasing: cubic-bezier(0.445, 0.05, 0.55, 0.95);
 
 .section__skill {
   background-color: var(--clr-bg);
+}
+
+.learning {
+  background-color: var(--clr-bg-alt);
+  transition: none !important;
+  transform: none !important;
 }
 </style>
